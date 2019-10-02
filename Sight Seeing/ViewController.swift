cc49087ae2fcc3a66a30f53eda2baea2d3172f4b
Jason Stiesi <jstiesi12683@icloud.com>
2019-10-02 14:53:48 -0400

    Initial Commit

---
+++ Sight Seeing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sight Seeing
+//
+//  Created by developer on 10/2/19.
+//  Copyright © 2019 developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
